d684317ff1eaf4fc37abbd6c5197e5780be3120e
jind0ri <samanthavinesscruz@gmail.com>
2026-04-15 01:10:34 +0800

    frontend initial commit

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,12 +1,20 @@
 # database.py
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-from app.config import DATABASE_URL
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:MatriqDB123$@db.aqumkaymehrylrvsgdpq.supabase.co:5432/postgres"
 
-engine = create_engine(DATABASE_URL)
+# Create the SQLAlchemy engine
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+# Create a configured "Session" class
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+# Create a Base class for models to inherit from
+Base = declarative_base()
+
+# Dependency to get the database session
 def get_db():
     db = SessionLocal()
     try:
