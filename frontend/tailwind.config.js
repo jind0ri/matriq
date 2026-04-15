4699c3926850c00a43c87a1769417533de8878e1
jind0ri <samanthavinesscruz@gmail.com>
2026-04-15 13:29:38 +0800

    fixed tailwind error

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
+  content: [
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}",
+  ],
+  theme: { extend: {} },
   plugins: [],
-}
-
+};
